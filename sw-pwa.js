5114086c6c6c3e69abcd1eee2977e5c6b0354704
anosg93-jpg <anosg93@gmail.com>
2026-07-06 20:42:28 +0300

    Add files via upload

--- sw-pwa.js
+++ sw-pwa.js
@@ -1,21 +1,27 @@
-const CACHE_NAME = 'loqma-v1';
+const CACHE_NAME = 'loqma-v1.0.1'; // غير الرقم عند كل تحديث جذري للكود
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// تثبيت التطبيق وتخزين الملفات
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+// تحديث الكود القديم وحذف التخزين المؤقت
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
   );
 });
 
+// تشغيل التطبيق أوفلاين
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
