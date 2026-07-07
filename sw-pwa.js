47088390ed10680169f40953a38daf9b66679be7
anosg93-jpg <anosg93@gmail.com>
2026-07-07 03:01:15 +0300

    Add files via upload

--- sw-pwa.js
+++ sw-pwa.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'loqma-app-v2'; // قمنا بتغيير الإصدار لضمان التحديث
+const CACHE_NAME = 'loqma-app-v3';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
@@ -9,7 +9,6 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    // هذه الاستراتيجية تجلب التحديثات أولاً من الشبكة لضمان ظهور أي تعديل جديد
     event.respondWith(
         fetch(event.request).catch(() => {
             return caches.match(event.request);
